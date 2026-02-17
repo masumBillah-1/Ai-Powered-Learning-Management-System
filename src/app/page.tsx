7d3e8d2bec5d27735383127dc8857022922454df
bayjidmia <mdbayjidmia982@gmail.com>
2026-02-17 19:08:12 +0600

    added Ctasection SuccessSection and StepSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import PricingSection from "@/components/Pricingsection/Pricingsection";
+import CtaSection from "@/components/Ctasection/Ctasection";
+import StepSection from "@/components/StepSection/StepSection";
+import SuccessSection from "@/components/SuccessSection/SuccessSection";
 
 export default function Home() {
   return (
     <div>
-      <PricingSection></PricingSection>
+      <CtaSection></CtaSection>
+      <SuccessSection></SuccessSection>
+      <StepSection></StepSection>
     </div>
   );
 }
