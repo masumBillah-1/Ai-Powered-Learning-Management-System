0a2ba6a84f4219113990051621c513d85fef6236
Sayma-Shimu <ahmedsayma399@gmail.com>
2026-02-18 20:20:11 +0600

    learn section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 
+import FAQ from "@/components/ui/FAQ";
 import HeroSection from "../components/Home/HeroSection"
 import ImpactSection from "../components/Home/ImpactSection"
 import ProblemSolution from "../components/Home/ProblemSolution"
+import JoinNow from "@/components/ui/JoinNow";
+import LearnSection from "@/components/Home/LearnSection";
 
 export default function Home() {
   return (
-    
-      <div className="container mx-auto px-6 py-10">
-         <HeroSection/>
-         <ImpactSection/>
-         <ProblemSolution/>
+
+    <div className="container mx-auto px-6 py-10">
+      <HeroSection />
+      <ImpactSection />
+      <ProblemSolution />
+      <LearnSection/>
       <FAQ />
-    <JoinNow />
-      </div>
-  
+      <JoinNow />
+    </div>
+
 
   );
 }
