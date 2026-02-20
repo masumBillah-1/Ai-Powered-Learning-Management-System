490fe340d158120d163da1f4e3af5a424d572175
Masum Billah <mb6517640@gmail.com>
2026-02-20 10:04:52 +0600

    juma code plm solver

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Hind_Siliguri } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +26,7 @@ const hindSiliguri = Hind_Siliguri({
 
 export const metadata: Metadata = {
   title: "SmartLMS Pro : Smart Online LMS Platform",
-  description: "SmartLMS Pro learning management system designed to deliver smart, personalized, and interactive online education experiences for students and instructors.",
+  description: "SmartLMS Pro learning management system designed to deliver smart, personalized, and interactive online education experiences students and instructors.",
 };
 
 export default function RootLayout({
@@ -48,7 +53,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${hindSiliguri.variable} antialiased min-h-screen flex flex-col transition-colors duration-300`}
         style={{ fontFamily: 'var(--font-hind-siliguri), var(--font-geist-sans), sans-serif' }}
       >
-        {children}
+          <Navbar />
+          {/* <FAQ /> */}
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer />
       </body>
     </html>
   );
