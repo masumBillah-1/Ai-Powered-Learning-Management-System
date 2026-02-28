8e0a3280a7e2ca59812eca82a3ec96ba4c692002
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-02-28 11:44:20 +0600

    save local changes

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -6,12 +6,12 @@ import BootcampFeatures from "@/components/Ctasection/Ctasection";
 import StepSection from "@/components/StepSection/StepSection";
 import SuccessSection from "@/components/SuccessSection/SuccessSection";
 import LearnSection from "@/components/Home/LearnSection";
-import HeroSections from "@/components/mainhome/HeroSections";
+
 
 export default function Home() {
   return (
     <div>
-      <HeroSections />
+
       <div className="container mx-auto px-6 py-10">
         <ImpactSection />
         <ProblemSolution />
