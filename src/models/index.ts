6f0b566c0a046d961711342bb515e650193df69f
Masum Billah <mb6517640@gmail.com>
2026-03-13 20:04:43 +0600

    project update

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,19 +5,20 @@ import mongoose from "mongoose";
 
 // Import all models to ensure they're registered
 import User from "./User";
-// import Course from "./Course";
 import Course from "./Course";
+import Blog from "./Blog";
 import Enrollment from "./Enrollment";
 import Transaction from "./Transaction";
 import Message from "./Message";
 import Notification from "./Notification";
 
 // Export models
-export { User, Course, Enrollment, Transaction, Message, Notification };
+export { User, Course, Blog, Enrollment, Transaction, Message, Notification };
 
 // Export types
 export type { IUserDocument } from "./User";
 export type { ICourseDocument, ILesson, IModule, IFAQ } from "./Course";
+export type { IBlog } from "./Blog";
 export type { IEnrollmentDocument } from "./Enrollment";
 export type { ITransactionDocument } from "./Transaction";
 export type { IMessageDocument } from "./Message";
@@ -25,7 +26,7 @@ export type { INotificationDocument } from "./Notification";
 
 // Helper function to ensure all models are registered
 export function ensureModelsRegistered() {
-  const models = { User, Course, Enrollment, Transaction, Message, Notification };
+  const models = { User, Course, Blog, Enrollment, Transaction, Message, Notification };
 
   console.log("📋 Registered Models:", Object.keys(mongoose.models));
 
@@ -35,6 +36,7 @@ export function ensureModelsRegistered() {
 export default {
   User,
   Course,
+  Blog,
   Enrollment,
   Transaction,
   Message,
