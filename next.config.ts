1648e7f66d3d23a2e45f75584b9fce3e36e7905e
Masum Billah <mb6517640@gmail.com>
2026-03-15 13:18:28 +0600

    project plm solved

--- next.config.ts
+++ next.config.ts
@@ -6,22 +6,6 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.pravatar.cc",
       },
-      {
-        protocol: "https",
-        hostname: "www.cdmi.in",
-      },
-      {
-        protocol: "https",
-        hostname: "cdmi.in",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.googleapis.com",
-      },
     ],
     // include both 75 and 92 since some images use quality 92
     qualities: [75, 92],
@@ -29,4 +13,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
